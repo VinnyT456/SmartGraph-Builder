a243294287a64622976967da245b801fd08e37e2
Vinny456 <vincent03280608@gmail.com>
2025-10-11 21:41:30 -0500

    Finalized the select graph feature and added images to the window

--- sections/graph_parameter.py
+++ sections/graph_parameter.py
@@ -1,29 +1,81 @@
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QKeySequence, QPixmap, QShortcut
 from PyQt6.QtWidgets import (
     QDialog, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QWidget, QVBoxLayout
 )
+import pandas as pd
 
 graph_module = ''
+selected_graph = ''
+idx = 0
 
 SEABORN_PLOTS = {
-    "Scatter Plot":{},
-    "Line Plot":{},
-    "Regression Plot":{},
-    "Bar Plot":{},
-    "Count Plot":{},
-    "Box Plot":{},
-    "Violin Plot":{},
-    "Swarm Plot":{},
-    "Strip Plot":{},
-    "Histogram":{},
-    "KDE Plot":{},
-    "ECDF Plot":{},
-    "Rug Plot":{},
-    "Heatmap":{},
-    "Pair Plot":{},
-    "Joint Plot":{},
-    "Cluster Map":{},
+    "Scatter Plot":{
+        "Image":"sample_graphs/scatter_plot.png"
+    },
+    "Line Plot":{
+        "Image":"sample_graphs/line_plot.png"
+    },
+    "Regression Plot":{
+        "Image":"sample_graphs/regression_plot.png"
+    },
+    "Bar Plot":{
+        "Image":"sample_graphs/bar_plot.png"
+    },
+    "Count Plot":{
+        "Image":"sample_graphs/count_plot.png"
+    },
+    "Box Plot":{
+        "Image":"sample_graphs/box_plot.png"
+    },
+    "Violin Plot":{
+        "Image":"sample_graphs/violin_plot.png"
+    },
+    "Swarm Plot":{
+        "Image":"sample_graphs/swarm_plot.png"
+    },
+    "Strip Plot":{
+        "Image":"sample_graphs/strip_plot.png"
+    },
+    "Histogram":{
+        "Image":"sample_graphs/histogram.png"
+    },
+    "KDE Plot":{
+        "Image":"sample_graphs/kde_plot.png"
+    },
+    "ECDF Plot":{
+        "Image":"sample_graphs/ecdf_plot.png"
+    },
+    "Rug Plot":{
+        "Image":"sample_graphs/rug_plot.png"
+    },
+    "Heatmap":{
+        "Image":"sample_graphs/heatmap.png"
+    },
+    "Pair Plot":{
+        "Image":"sample_graphs/pair_plot.png"
+    },
+    "Joint Plot":{
+        "Image":"sample_graphs/joint_plot.png"
+    },
+    "Cluster Map":{
+        "Image":"sample_graphs/cluster_map.png"
+    },
 }
+
+class graph_parameter_table(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("""
+            QWidget {
+                border-radius: 24px;
+                background: white;
+                border: 2px solid black;
+                margin: 10px;
+                padding: 10px;           
+            }
+        """)
+        self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
 
 class select_graph_module(QPushButton):
     def __init__(self):
@@ -77,8 +129,11 @@ class select_graph_module(QPushButton):
         graph_module = self.graph_module[self.graph_module_idx]
 
 class select_graph_window(QDialog):
-    def __init__(self):
+    def __init__(self,available_graphs,graph_images):
         super().__init__()
+
+        self.available_graphs = available_graphs
+        self.graph_images = graph_images
 
         self.setWindowTitle("Select Your Graph")
         self.setFixedHeight(500)
@@ -96,7 +151,7 @@ class select_graph_window(QDialog):
             color: black;
         """)
 
-        self.graph_label = QLabel("Graph Name")
+        self.graph_label = QLabel(f"{self.available_graphs[idx%len(self.available_graphs)]}")
         self.graph_label.setWordWrap(True)
         self.graph_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.graph_label.setStyleSheet("""
@@ -134,8 +189,20 @@ class select_graph_window(QDialog):
             }
         """)
 
+        self.image_label = QLabel(self.graph_image)
+        image = QPixmap(f"./{self.graph_images[idx]}")
+        self.image_label.setPixmap(image)
+        self.image_label.setScaledContents(True)
+
+        layout = QVBoxLayout(self.graph_image)
+        layout.setContentsMargins(0, 0, 0, 0) 
+        layout.addWidget(self.image_label)
+        self.graph_image.setLayout(layout)
+
         self.select_button = QPushButton("Select")
         self.select_button.setObjectName("Select_Button")
+        enter_shortcut = QShortcut(QKeySequence("Return"), self) 
+        enter_shortcut.activated.connect(self.select_button.click)  
         self.select_button.setStyleSheet("""
             QPushButton#Select_Button{  
                 background: white;
@@ -160,7 +227,10 @@ class select_graph_window(QDialog):
         """)
 
         self.previous_graph = QPushButton()
+        self.previous_graph.setShortcut("Left")
         self.previous_graph.setObjectName("Previous_Graph")
+        left_shortcut = QShortcut(QKeySequence("Left"), self) 
+        left_shortcut.activated.connect(self.previous_graph.click)
         self.previous_graph.setStyleSheet("""
             QPushButton#Previous_Graph{  
                 background: white;
@@ -189,6 +259,8 @@ class select_graph_window(QDialog):
         """)
 
         self.next_graph = QPushButton()
+        right_shortcut = QShortcut(QKeySequence("Right"), self) 
+        right_shortcut.activated.connect(self.next_graph.click)
         self.next_graph.setObjectName("Next_Graph")
         self.next_graph.setStyleSheet("""
             QPushButton#Next_Graph{  
@@ -239,6 +311,36 @@ class select_graph_window(QDialog):
 
         self.setLayout(main_layout)
 
+        self.previous_graph.clicked.connect(self.view_previous_graph)
+        self.next_graph.clicked.connect(self.view_next_graph)
+        self.select_button.clicked.connect(self.select_graph)
+
+    def view_previous_graph(self):
+        global idx
+        idx -= 1
+        idx %= len(self.available_graphs)
+        self.graph_label.setText(self.available_graphs[idx])
+        self.update_graph_image()
+
+    def view_next_graph(self):
+        global idx
+        idx -= 1
+        idx %= len(self.available_graphs)
+        self.graph_label.setText(self.available_graphs[idx])
+        self.update_graph_image()
+
+    def select_graph(self):
+        global selected_graph
+        selected_graph = self.available_graphs[idx]
+        self.close() 
+
+    def update_graph_image(self):
+        image_path = self.graph_images[idx]
+        pixmap = QPixmap(image_path)
+        if pixmap.isNull():
+            print(f"Failed to load image: {image_path}")
+        self.image_label.setPixmap(pixmap)
+
 class select_graph(QPushButton):
     def __init__(self):
         super().__init__()
@@ -253,7 +355,7 @@ class select_graph(QPushButton):
                     );
             color: black;
         """)
-
+    
         self.label = QLabel("Select Type of Graph")
         self.label.setWordWrap(True)
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -275,7 +377,16 @@ class select_graph(QPushButton):
 
     def open_select_graph_window(self):
         if (graph_module != ''):
-            select_graph_window().exec()
+            graph_images = [i["Image"] for i in list(SEABORN_PLOTS.values())]
+            select_graph_window(list(SEABORN_PLOTS.keys()),graph_images).exec()
+            if (selected_graph != ''):
+                self.label.setText(selected_graph)
+                self.label.setStyleSheet("""
+                    font-family: "SF Pro Display";
+                    font-weight: 600;
+                    font-size: 18px;
+                    border-radius: 16px;
+                """) 
 
 class GraphParameter_TopBar(QWidget):
     def __init__(self):
@@ -307,12 +418,14 @@ class GraphParameter_Section(QWidget):
         """)
 
         self.setFixedWidth(350)
+
+        self.graph_parameter_table = graph_parameter_table()
         
         layout = QVBoxLayout()
         layout.addWidget(GraphParameter_TopBar())
-        layout.addStretch()
+        layout.addWidget(self.graph_parameter_table)
         layout.setContentsMargins(0,0,0,0) 
-        layout.setSpacing(0)
+        layout.setSpacing(5)
 
         self.setLayout(layout)
 
