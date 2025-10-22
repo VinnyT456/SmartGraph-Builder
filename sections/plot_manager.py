ed08cde01626004b733e8fa4ae61f28f7a367007
Vinny456 <vincent03280608@gmail.com>
2025-10-21 20:43:31 -0500

    Updated the plot manager to update the bbox_anchor argument in real time

--- sections/plot_manager.py
+++ sections/plot_manager.py
@@ -40,7 +40,10 @@ class PlotManager:
         self.db.update({"title":title},Query().version == self.current_version)
 
     def update_legend_loc(self,new_position):
-        self.db.update(lambda db: db["legend"].update({'loc': new_position}) or db,Query().version == self.current_version)
+        self.db.update(lambda db: db["legend"].update({"loc": new_position}) or db,Query().version == self.current_version)
+
+    def update_legend_bbox_anchor(self,new_bbox_anchor):
+        self.db.update(lambda db: db["legend"].update({"bbox_to_anchor":new_bbox_anchor}) or db,Query().version == self.current_version)
 
     def insert_plot_parameter(self, plot_data):
         plot_copy = plot_data.copy()
