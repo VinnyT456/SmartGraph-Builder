6c04f20479e6ae8827dee420e9f93a39c3369515
Vinny456 <vincent03280608@gmail.com>
2025-10-21 19:52:23 -0500

    Updated the plot manager to update the loc arguments in real time

--- sections/plot_manager.py
+++ sections/plot_manager.py
@@ -39,6 +39,9 @@ class PlotManager:
     def update_title(self,title):
         self.db.update({"title":title},Query().version == self.current_version)
 
+    def update_legend_loc(self,new_position):
+        self.db.update(lambda db: db["legend"].update({'loc': new_position}) or db,Query().version == self.current_version)
+
     def insert_plot_parameter(self, plot_data):
         plot_copy = plot_data.copy()
         plot_copy["version"] = self.current_version + 1
