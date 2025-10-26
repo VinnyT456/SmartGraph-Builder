2381c88df741efcb9815ae996685d50cceb5e2c9
Vinny456 <vincent03280608@gmail.com>
2025-10-25 23:43:12 -0500

    Added the framealpha adjustment section

--- sections/buttons.py
+++ sections/buttons.py
@@ -1522,7 +1522,7 @@ class ncol_adjustment_section(QWidget):
         self.ncol_input.setFixedHeight(60)
 
         #Connect any changes with the text to an update function
-        self.ncol_input.textChanged.connect(self.update_ncol)
+        self.ncol_input.textChanged.connect(self.change_ncol)
 
         #Create two widget to display valid and invalid inputs
         self.valid_input_widget = QWidget()
@@ -1626,7 +1626,7 @@ class ncol_adjustment_section(QWidget):
 
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
     
-    def update_ncol(self):
+    def change_ncol(self):
         #Extract the ncol input from the user and remove any excess text from it
         ncol_input = self.ncol_input.text().strip()
 
@@ -4894,6 +4894,190 @@ class edge_color_adjustment_section(QWidget):
             self.grayscale_input.clearFocus()
         super().mousePressEvent(event)
 
+class framealpha_adjustment_section(QWidget):
+    def __init__(self,selected_graph):
+        super().__init__()
+        
+        self.plot_manager = PlotManager()
+        
+        self.selected_graph = selected_graph
+
+        #Create a section to display the loc section and style it
+        self.framealpha_adjustment_section = QWidget()
+        self.framealpha_adjustment_section.setObjectName("framealpha_adjustment_section")
+        self.framealpha_adjustment_section.setStyleSheet("""
+            QWidget#framealpha_adjustment_section{
+                background: qlineargradient(
+                    x1:0, y1:0, x2:1, y2:0,
+                    stop:0 #f5f5ff,
+                    stop:0.5 #f7f5fc,
+                    stop:1 #f0f0ff
+                );
+                border: 2px solid black;
+                border-radius: 24px;
+            }
+            QLineEdit{
+                background: qlineargradient(
+                    x1:0, y1:0, x2:1, y2:0,
+                    stop:0 #f5f5ff,
+                    stop:0.5 #f7f5fc,
+                    stop:1 #f0f0ff
+                );
+                color: black;
+                font-size: 24pt;
+                border: 2px solid black;
+                border-radius: 24px;
+            }
+        """)
+
+        #Initialize the ncol value to be 0
+        self.framealpha_value = None
+
+        #Create a line edit object for the user to input the ncol
+        self.framealpha_input = QLineEdit()
+        self.framealpha_input.setPlaceholderText("framealpha: ")
+
+        #Set the height of the line edit object to make it look good
+        self.framealpha_input.setFixedHeight(60)
+
+        #Connect any changes with the text to an update function
+        self.framealpha_input.textChanged.connect(self.change_framealpha)
+
+        #Create two widget to display valid and invalid inputs
+        self.valid_input_widget = QWidget()
+        self.valid_input_widget.setObjectName("valid_input")
+        self.valid_input_widget.setStyleSheet("""
+            QWidget#valid_input{
+                background: qlineargradient(
+                    x1:0, y1:0, x2:1, y2:0,
+                    stop:0 rgba(94, 255, 234, 1),   
+                    stop:0.3 rgba(63, 252, 180, 1), 
+                    stop:0.6 rgba(150, 220, 255, 1)
+                    stop:1 rgba(180, 200, 255, 1)  
+                );
+                border: 2px solid black;
+                border-radius: 16px;
+            }
+        """)
+
+        self.valid_input_label = QLabel("Valid Input")
+        self.valid_input_label.setWordWrap(True)
+        self.valid_input_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.valid_input_label.setObjectName("valid_input_label")
+        self.valid_input_label.setStyleSheet("""
+            QLabel#valid_input_label{
+                font-family: "SF Pro Display";
+                font-weight: 600;
+                font-size: 24px;
+                padding: 6px;
+                color: black;
+                border: none;
+                background: transparent;
+            }
+        """)
+
+        valid_input_layout = QVBoxLayout(self.valid_input_widget)
+        valid_input_layout.addWidget(self.valid_input_label)
+        valid_input_layout.setSpacing(0)
+        valid_input_layout.setContentsMargins(0,0,0,0)
+
+        self.invalid_input_widget = QWidget()
+        self.invalid_input_widget.setObjectName("invalid_input")
+        self.invalid_input_widget.setStyleSheet("""
+            QWidget#invalid_input{
+                background: qlineargradient(
+                    x1:0, y1:0, x2:1, y2:0,
+                    stop:0 rgba(255, 100, 100, 1),   
+                    stop:0.4 rgba(255, 130, 120, 1), 
+                    stop:0.7 rgba(200, 90, 150, 1), 
+                    stop:1 rgba(180, 60, 140, 1)     
+                );
+                border: 2px solid black;
+                border-radius: 16px;
+            }
+        """)
+
+        self.invalid_input_label = QLabel("Invalid Input")
+        self.invalid_input_label.setWordWrap(True)
+        self.invalid_input_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.invalid_input_label.setObjectName("invalid_input_label")
+        self.invalid_input_label.setStyleSheet("""
+            QLabel#invalid_input_label{
+                font-family: "SF Pro Display";
+                font-weight: 600;
+                font-size: 24px;
+                padding: 6px;
+                color: black;
+                border: none;
+                background: transparent;
+            }
+        """)
+
+        invalid_input_layout = QVBoxLayout(self.invalid_input_widget)
+        invalid_input_layout.addWidget(self.invalid_input_label)
+        invalid_input_layout.setSpacing(0)
+        invalid_input_layout.setContentsMargins(0,0,0,0)
+
+        self.valid_input_widget.setMaximumHeight(50)
+        self.invalid_input_widget.setMaximumHeight(50)
+
+        self.valid_input_widget.hide()
+        self.invalid_input_widget.hide()
+
+        #Create a layout for the ncol adjustment section and add the line edit object to it
+        framealpha_section_layout = QVBoxLayout(self.framealpha_adjustment_section)
+        framealpha_section_layout.addWidget(self.framealpha_input)
+        framealpha_section_layout.addWidget(self.valid_input_widget)
+        framealpha_section_layout.addWidget(self.invalid_input_widget)
+    
+        #Add the margins, spacing, and stretch to the layout to make it look good
+        framealpha_section_layout.setContentsMargins(10,10,10,10)
+        framealpha_section_layout.setSpacing(10)
+        framealpha_section_layout.addStretch()
+
+        #Add the ncol adjustment section to the main widget
+        main_layout = QVBoxLayout(self)
+        main_layout.addWidget(self.framealpha_adjustment_section)
+        
+        #Set both the spacing and margins for the main widget to make sure it fits nicely
+        main_layout.setSpacing(0)
+        main_layout.setContentsMargins(0,0,0,0)
+
+        self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
+    
+    def change_framealpha(self):
+        #Extract the ncol input from the user and remove any excess text from it
+        framealpha_input = self.framealpha_input.text().strip()
+
+        if (framealpha_input == ""):
+            self.valid_input_widget.hide()
+            self.invalid_input_widget.hide()
+            self.framealpha_value = None
+            return
+
+        #Only update the ncol value in the json file if the input is valid
+        try:
+            framealpha_input = float(framealpha_input)
+            if (0 > framealpha_input or framealpha_input > 1):
+                raise Exception
+            self.valid_input_widget.show()
+            self.invalid_input_widget.hide()
+        except:
+            self.valid_input_widget.hide()
+            self.invalid_input_widget.show()
+        else:
+            self.framealpha_value = framealpha_input
+            self.update_framealpha()
+
+    def update_framealpha(self):
+        db = self.plot_manager.get_db()
+        if (db != []):
+            self.plot_manager.update_legend("framealpha",self.framealpha_value)
+        else:
+            plot_parameters = plot_json[self.selected_graph].copy()
+            plot_parameters["legend"]["framealpha"] = self.framealpha_value
+            self.plot_manager.insert_plot_parameter(plot_parameters)
+
 class legend_button(QDialog):
     def __init__(self,selected_graph):
         super().__init__()
@@ -4983,7 +5167,7 @@ class legend_button(QDialog):
         self.layout = QHBoxLayout(self)
         self.layout.addWidget(self.scroll_section,stretch=1)
         self.layout.addSpacing(10)
-        self.layout.addWidget(edge_color_adjustment_section(self.selected_graph),stretch=1)
+        self.layout.addWidget(framealpha_adjustment_section(self.selected_graph),stretch=1)
 
         #Create a shortcut for the user to go to the previous column by press up
         up_shortcut = QShortcut(QKeySequence("up"), self) 
