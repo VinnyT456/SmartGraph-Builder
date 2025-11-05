6276e89f03689fe98cfb24d801608a7d6a4f1bbc
Vinny456 <vincent03280608@gmail.com>
2025-11-04 20:08:42 -0600

    Updated the plot manager to update the hue and markers

--- sections/plot_manager.py
+++ sections/plot_manager.py
@@ -55,6 +55,9 @@ class PlotManager:
     def update_grid(self,grid):
         self.db.update({"grid":grid},Query().version == self.current_version)
 
+    def update_hue(self,hue):
+        self.db.update({"hue":hue},Query().version == self.current_version)
+
     def insert_plot_parameter(self, plot_data):
         plot_copy = plot_data.copy()
         plot_copy["version"] = self.current_version + 1
