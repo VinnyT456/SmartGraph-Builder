803f196a158655fa6f64669e8e3b8e618fdee6fc
Vinny456 <vincent03280608@gmail.com>
2025-12-02 17:34:45 -0600

    Created a new function to update the style

--- sections/plot_manager.py
+++ sections/plot_manager.py
@@ -58,6 +58,9 @@ class PlotManager:
     def update_hue(self,hue):
         self.db.update({"hue":hue},Query().version == self.current_version)
 
+    def update_style(self,style):
+        self.db.update({"style":style},Query().version == self.current_version)
+
     def insert_plot_parameter(self, plot_data):
         plot_copy = plot_data.copy()
         plot_copy["version"] = self.current_version + 1
