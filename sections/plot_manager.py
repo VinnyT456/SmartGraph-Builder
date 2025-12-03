3af721d5255c91836e511a51f606756a252389d6
Vinny456 <vincent03280608@gmail.com>
2025-12-02 21:46:20 -0600

    Created two functions to update the size and sizes

--- sections/plot_manager.py
+++ sections/plot_manager.py
@@ -61,6 +61,12 @@ class PlotManager:
     def update_style(self,style):
         self.db.update({"style":style},Query().version == self.current_version)
 
+    def update_size(self,size):
+        self.db.update({"size":size},Query().version == self.current_version)
+
+    def update_sizes(self,sizes):
+        self.db.update({"sizes":sizes},Query().version == self.current_version)
+
     def insert_plot_parameter(self, plot_data):
         plot_copy = plot_data.copy()
         plot_copy["version"] = self.current_version + 1
