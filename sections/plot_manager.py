37b8061caae4274f3f0b5f8e9b181bc150696a6f
Vinny456 <vincent03280608@gmail.com>
2026-01-07 23:27:01 -0600

    Created a function for updating the alpha

--- sections/plot_manager.py
+++ sections/plot_manager.py
@@ -71,6 +71,9 @@ class PlotManager:
     def update_palette(self,palette):
         self.db.update({"palette":palette},Query().version == self.current_version)
 
+    def update_alpha(self,alpha):
+        self.db.update({"alpha":alpha},Query().version == self.current_version)
+
     def insert_plot_parameter(self, plot_data):
         plot_copy = plot_data.copy()
         plot_copy["version"] = self.current_version + 1
